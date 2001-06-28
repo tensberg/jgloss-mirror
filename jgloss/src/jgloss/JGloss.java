3a393fb5976a0707ebf227b929285e5770a6f712
Michael Koch <tensberg@gmx.net>
2001-06-28 18:34:36 +0000

    better handling of initialization errors

--- jgloss/src/jgloss/JGloss.java
+++ jgloss/src/jgloss/JGloss.java
@@ -29,6 +29,8 @@ import jgloss.ui.*;
 import java.io.*;
 import java.util.ResourceBundle;
 import java.text.MessageFormat;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.UIManager;
 
@@ -107,83 +109,162 @@ public class JGloss {
      * @param args Arguments to the application. Not used.
      * @exception Exception if something goes wrong during the initialisation.
      */
-    public static void main( String args[]) throws Exception {
-        if (args.length > 0) {
-            if (args[0].equals( "-h") || args[0].equals( "--help") ||
-                args[0].equals( "/?")) {
-                System.err.println( messages.getString( "main.usage"));
-                System.exit( 0);
-            }
-            else if (args[0].equals( "-i") || args[0].equals( "--createindex")) {
-                for ( int i=1; i<args.length; i++) {
-                    EDict e = new EDict( args[i], false);
-                    e.buildIndex();
-                    try {
-                        // write index to local directory
-                        e.saveJJDX( new File( e.getName() + EDict.JJDX_EXTENSION));
-                    } catch (IOException ex) {
-                        System.err.println( messages.getString( "edict.error.writejjdx",
-                                                                new String[] { ex.getClass().getName(),
-                                                                               ex.getLocalizedMessage() }));
-                        System.exit( 1);
-                    }
-                }
-                System.exit( 0);
-            }
-            else if (args[0].startsWith( "-") || args[0].startsWith( "/")) {
-                System.err.println( messages.getString( "main.unknownoption",
-                                                        new String[] { args[0] }));
-                System.err.println( messages.getString( "main.usage"));
-                System.exit( 1);
-            }
-        }
-
-        UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName());
-
-        SplashScreen splash = new SplashScreen();
-        splash.setInfo( messages.getString( "splashscreen.loadingPreferences"));
+    public static void main( String args[]) {
         try {
-            prefs.load();
-        } catch (IOException ex) {
-            displayError( messages.getString( "error.loadPreferences"), ex);
-        }
-
-        splash.setInfo( messages.getString( "splashscreen.initPreferences"));
-        // Initialize the preferences at startup. This includes loading the dictionaries.
-        PreferencesFrame.getFrame();
-        splash.setInfo( messages.getString( "splashscreen.initMain"));
-        if (args.length == 0)
-            new JGlossFrame();
-        else
-            for ( int i=0; i<args.length; i++) {
-                JGlossFrame f = new JGlossFrame();
-                f.loadDocument( args[i]);
+            if (args.length > 0) {
+                if (args[0].equals( "-h") || args[0].equals( "--help") ||
+                    args[0].equals( "/?")) {
+                    System.err.println( messages.getString( "main.usage"));
+                    System.exit( 0);
+                }
+                else if (args[0].equals( "-i") || args[0].equals( "--createindex")) {
+                    for ( int i=1; i<args.length; i++) {
+                        EDict e = new EDict( args[i], false);
+                        e.buildIndex();
+                        try {
+                            // write index to local directory
+                            e.saveJJDX( new File( e.getName() + EDict.JJDX_EXTENSION));
+                        } catch (IOException ex) {
+                            System.err.println( messages.getString
+                                                ( "edict.error.writejjdx",
+                                                  new String[] { ex.getClass().getName(),
+                                                                 ex.getLocalizedMessage() }));
+                            System.exit( 1);
+                        }
+                    }
+                    System.exit( 0);
+                }
+                else if (args[0].startsWith( "-") || args[0].startsWith( "/")) {
+                    System.err.println( messages.getString( "main.unknownoption",
+                                                            new String[] { args[0] }));
+                    System.err.println( messages.getString( "main.usage"));
+                    System.exit( 1);
+                }
             }
-        splash.close();
+
+            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName());
+            
+            SplashScreen splash = new SplashScreen();
+            splash.setInfo( messages.getString( "splashscreen.loadingPreferences"));
+            try {
+                prefs.load();
+            } catch (IOException ex) {
+                displayError( messages.getString( "error.loadPreferences"), ex, false);
+            }
+            
+            splash.setInfo( messages.getString( "splashscreen.initPreferences"));
+            // Initialize the preferences at startup. This includes loading the dictionaries.
+            PreferencesFrame.getFrame();
+            splash.setInfo( messages.getString( "splashscreen.initMain"));
+            if (args.length == 0)
+                new JGlossFrame();
+            else
+                for ( int i=0; i<args.length; i++) {
+                    JGlossFrame f = new JGlossFrame();
+                    f.loadDocument( args[i]);
+                }
+            splash.close();
+        } catch (NoClassDefFoundError ex) {
+            displayError( messages.getString( "error.noclassdef"), ex, true);
+            System.exit( 1);
+        } catch (NoSuchMethodError ex) {
+            displayError( messages.getString( "error.noclassdef"), ex, true);
+            System.exit( 1);
+        } catch (ClassNotFoundException ex) {
+            displayError( messages.getString( "error.noclassdef"), ex, true);
+            System.exit( 1);
+        } catch (Exception ex) {
+            displayError( messages.getString( "error.initialization.generic"), ex, true);
+            System.exit( 1);
+        }
     }
 
     /**
      * Exits JGloss. Before the application quits, the preferences will be saved.
-     * This method will be called when the last JGloss frame is closed.
+     * This method will be called when the last JGloss frame is closed. The method may return
+     * before the application quits to allow event processing to take place if it triggers an
+     * error dialog.
      */
     public static void exit() {
-        try {
-            prefs.store();
-        } catch (IOException ex) {
-            displayError( messages.getString( "error.storePreferences"), ex);
-        }
-        System.exit( 0);
+        // Instantiate a new Thread because the exit() method may have been called from
+        // an event dispatch thread (for example a window close event). The displayError method which might
+        // be called needs the event dispatch thread to work, so the exit() method has to return.
+        new Thread() {
+                public void run() {
+                    try {
+                        prefs.store();
+                    } catch (IOException ex) {
+                        displayError( messages.getString( "error.storePreferences"), ex, false);
+                    }
+                    System.exit( 0);
+                }
+            }.start();
     }
 
     /**
-     * Shows an error message. Currenty it is written to System.err.
+     * Shows an error message to the user.
      *
      * @param message A user-understandable error message.
      * @param t The exception which signalled the error.
+     * @param fatal <code>true</code> if the application cannot be started because of the error.
      */
-    public static void displayError( String message, Throwable t) {
+    private static void displayError( String message, Throwable t, boolean fatal) {
         System.err.println( message);
-        System.err.println( t.getLocalizedMessage());
+        t.printStackTrace();
+
+        // Display error message in a frame. Note that this needs at least Java 1.1
+        final Frame f = new Frame( messages.getString( "error.initialization.title"));
+        f.setLayout( new BorderLayout());
+        
+        String msg = messages.getString( "error.initialization", new String[] {
+            fatal ? messages.getString( "error.initialization.fatal") : "",
+            message, t.getClass().getName(), t.getLocalizedMessage() });
+        
+        int rows = 1;
+        int maxcols = 0;
+        int cols = 0;
+        for ( int i=0; i<msg.length(); i++) {
+            if (msg.charAt( i) == '\n') {
+                rows++;
+                if (maxcols < cols)
+                    maxcols = cols;
+                cols = 0;
+            }
+            else
+                cols++;
+        }
+        if (maxcols < cols)
+            maxcols = cols;
+        
+        TextArea a = new TextArea
+            ( msg, rows, maxcols,
+              TextArea.SCROLLBARS_NONE);
+        a.setFont( new Font( "Dialog", Font.PLAIN, 12));
+        a.setEditable( false);
+        f.add( a, BorderLayout.CENTER);
+        
+        Button ok = new Button( messages.getString( "button.ok"));
+        ok.addActionListener( new ActionListener() {
+                public void actionPerformed( ActionEvent e) {
+                    synchronized (f) {
+                        f.hide();
+                        f.dispose();
+                        f.notify();
+                    }
+                }
+            });
+        f.add( ok, BorderLayout.SOUTH);
+        f.pack();
+        
+        Dimension d = f.getToolkit().getScreenSize();
+        f.setLocation( (d.width-f.getWidth())/2, (d.height-f.getHeight())/2);
+        
+        f.show();
+        synchronized (f) {
+            try {
+                f.wait();
+            } catch (InterruptedException ex) {}
+        }
     }
 
     /**
