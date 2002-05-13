637f176335e87ae2622464d96b73491aac0426d8
Michael Koch <tensberg@gmx.net>
2002-05-13 20:13:03 +0000

    *** empty log message ***

--- jgloss/src/jgloss/ui/export/XMLExporter.java
+++ jgloss/src/jgloss/ui/export/XMLExporter.java
@@ -44,13 +44,38 @@ import javax.swing.text.html.*;
  * @author Michael Koch
  */
 public class XMLExporter extends JGlossWriter {
+    /**
+     * Name of the root (or document) element of the generated XML file.
+     */
     public static final String ROOT_ELEMENT_NAME = "jgloss";
     
+    /**
+     * Encoding used for the XML document.
+     */
     public final static String ENCODING_UTF8 = "UTF-8";
     
+    /**
+     * Set to <code>true</code> while the <code>head</code> element and its descendants are
+     * written.
+     */
     protected boolean inHead = false;
-    protected boolean writingEmptyTag = false;
 
+    /**
+     * If set to <code>true</code> all text until the next closing tag and this tag 
+     * will be ignored. The variable will be reset to <code>false</code> when the closing tag is
+     * encountered.
+     */
+    protected boolean skipElement = false;
+
+    /**
+     * Creates an XML exporter which will write the given JGloss document to the writer in XML
+     * format.
+     *
+     * @param out The writer to which the document is written. The encoding declaration of
+     *            the generated document always specifies "UTF-8", if the writer writes to an
+     *            output stream, this encoding must be used.
+     * @param doc The JGloss document which is to be exported as XML.
+     */
     public XMLExporter( Writer out, JGlossDocument doc) {
         super( out, ENCODING_UTF8, doc);
     }
@@ -58,8 +83,10 @@ public class XMLExporter extends JGlossWriter {
     protected void startTag( Element elem) throws IOException, BadLocationException {
         AttributeSet attr = elem.getAttributes();
         if (matchNameAttribute( attr, HTML.Tag.HTML))
+            // write XML header and ROOT_ELEMENT_NAME instead of HTML element
             writeProlog();
         else if (matchNameAttribute( attr, HTML.Tag.HEAD)) {
+            // filter unwanted elements from header and add custom elements.
             super.startTag( elem);
             writeHeadInfo();
             inHead = true;
@@ -78,7 +105,11 @@ public class XMLExporter extends JGlossWriter {
     }
 
     protected void endTag( Element elem) throws IOException {
-        if (matchNameAttribute( elem.getAttributes(), HTML.Tag.HEAD)) {
+        /*if (skipElement) {
+            // skip the closing tag of the current element
+            skipElement = false;
+        }
+        else*/ if (matchNameAttribute( elem.getAttributes(), HTML.Tag.HEAD)) {
             inHead = false;
             super.endTag( elem);
         }
@@ -92,9 +123,36 @@ public class XMLExporter extends JGlossWriter {
         if (inHead)
             return;
 
-        super.emptyTag( elem);
+        AttributeSet attr = elem.getAttributes();
+        // apparently emptyTag is called for elements which can't have other elements as children
+        if ((attr.isDefined( AnnotationTags.READING) ||
+             attr.isDefined( AnnotationTags.TRANSLATION))
+            && elem.getEndOffset()-elem.getStartOffset() == 1 &&
+            doc.getText( elem.getStartOffset(), 1).equals( " ")) {
+            // Empty reading/translation tag. The single space is only there so that the
+            // HTMLDocument class does not remove the element completely.
+            // Output an empty element.
+            write( '<');
+            if (attr.isDefined( AnnotationTags.READING))
+                write( AnnotationTags.READING.getId());
+            else
+                write( AnnotationTags.TRANSLATION.getId());
+            write( " />");
+        }
+        else
+            super.emptyTag( elem);
     }
 
+    /*protected void write( char[] text, int off, int len) throws IOException {
+        if (skipElement) // don't write the text of the current element
+            return;
+
+        super.write( text, off, len);
+        }*/
+
+    /**
+     * Writes the XML declaration and the root element open tag.
+     */
     protected void writeProlog() throws IOException {
         write( "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
         writeLineSeparator();
@@ -102,11 +160,17 @@ public class XMLExporter extends JGlossWriter {
         writeLineSeparator();
     }
 
+    /**
+     * Writes the root element close tag.
+     */
     protected void writeEpilog() throws IOException {
         write( "</" + ROOT_ELEMENT_NAME + ">");
         writeLineSeparator();
     }
 
+    /**
+     * Writes additional information to the <code>head</code> section of the document.
+     */
     protected void writeHeadInfo() throws IOException {
         if (doc.getTitle() != null) {
             write( "<title>");
