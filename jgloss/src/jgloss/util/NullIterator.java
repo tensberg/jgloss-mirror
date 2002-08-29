ab2d4b63c8a75acee4f0171201b7aa3077b53900
Michael Koch <tensberg@gmx.net>
2002-08-29 13:37:46 +0000

    *** empty log message ***

---
+++ jgloss/src/jgloss/util/NullIterator.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2002 Michael Koch (tensberg@gmx.net)
+ *
+ * This file is part of JGloss.
+ *
+ * JGloss is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JGloss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JGloss; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ *
+ */
+
+package jgloss.util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator implementation without any elements. The singleton instance of this
+ * iterator can be used wherever an iteration over an empty collection is required.
+ */
+public class NullIterator implements Iterator {
+    public static final Iterator INSTANCE = new NullIterator();
+
+    private NullIterator() {}
+
+    public boolean hasNext() { return false; }
+
+    public Object next() throws NoSuchElementException { 
+        throw new NoSuchElementException();
+    }
+
+    public void remove() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+} // class NullIterator
---
+++ jgloss/src/jgloss/util/NumberTools.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2001,2002 Michael Koch (tensberg@gmx.net)
+ *
+ * This file is part of JGloss.
+ *
+ * JGloss is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * JGloss is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JGloss; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ *
+ */
+
+package jgloss.util;
+
+/**
+ * Utility methods for handling numbers.
+ *
+ * @author Michael Koch
+ */
+public class NumberTools {
+    private NumberTools() {}
+
+    /**
+     * Converts the byte value to an int with the value of the 8 bits
+     * interpreted as an unsigned byte.
+     *
+     * @param b The byte value to convert.
+     * @return The unsigned byte value of b.
+     */
+    public final static int byteToUnsignedByte( byte b) {
+        return b & 0xff;
+    }
+
+} // class NumberTools
