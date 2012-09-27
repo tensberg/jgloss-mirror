de08f9b1516e23b8ef96f43b42a5219ed0a50e33
Michael Koch <tensberg@gmx.net>
2012-09-27 11:40:19 +0200

    updated year in copyright note

--- jgloss-core/src/main/java/jgloss/dictionary/BaseEntry.java
+++ jgloss-core/src/main/java/jgloss/dictionary/BaseEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002-2004 Michael Koch (tensberg@gmx.net)
+ * Copyright (C) 2002-2012 Michael Koch (tensberg@gmx.net)
  *
  * This file is part of JGloss.
  *
--- jgloss/src/main/java/jgloss/ui/export/ConstantParameter.java
+++ jgloss/src/main/java/jgloss/ui/export/ConstantParameter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2004 Michael Koch (tensberg@gmx.net)
+ * Copyright (C) 2003-2012 Michael Koch (tensberg@gmx.net)
  *
  * This file is part of JGloss.
  *
