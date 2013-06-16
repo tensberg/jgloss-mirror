618c779e6dc21ed5c84b4361be18e120875421e1
Michael Koch <tensberg@gmx.net>
2013-06-16 10:32:15 +0200

    added home page button to empty document page

--- jgloss/src/main/java/jgloss/ui/gloss/EmptyDocumentActionsPanel.java
+++ jgloss/src/main/java/jgloss/ui/gloss/EmptyDocumentActionsPanel.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 
@@ -13,10 +12,13 @@ import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 
 import jgloss.JGlossApp;
+import jgloss.ui.util.HyperlinkAction;
 import jgloss.ui.util.Icons;
 import jgloss.ui.util.UIUtilities;
+import net.miginfocom.swing.MigLayout;
 
 /**
  * Panel shown in an empty JGloss document, offering the most important actions.
@@ -26,20 +28,20 @@ class EmptyDocumentActionsPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private static final ImageIcon BACKGROUND_IMAGE = Icons.getIcon("images/emptydocument_background.png");
-	
+
 	private final Action wordLookupAction = new AbstractAction() {
         private static final long serialVersionUID = 1L;
 
         {
         	UIUtilities.initAction( this, "main.menu.wordlookup");
         }
-        
+
 		@Override
 		public void actionPerformed( ActionEvent e) {
             JGlossApp.getLookupFrame().setVisible(true);
         }
     };
-	
+
 	EmptyDocumentActionsPanel(DocumentActions documentActions) {
 		setBackground(Color.WHITE);
 		setOpaque(true);
@@ -51,24 +53,28 @@ class EmptyDocumentActionsPanel extends JPanel {
 	protected void paintComponent(Graphics g) {
 	    super.paintComponent(g);
 	    Image image = BACKGROUND_IMAGE.getImage();
-	    
+
 	    int x = (getWidth() - BACKGROUND_IMAGE.getIconWidth())/2;
 	    int y = (getHeight() - BACKGROUND_IMAGE.getIconHeight())/2;
-	    
+
 		g.drawImage(image, x, y, null);
 	}
-	
+
 	private Component createActionsPanel(DocumentActions documentActions) {
 		JPanel actionsPanel = new JPanel(new GridBagLayout());
 		actionsPanel.setOpaque(false);
-	   	JPanel actionButtonsPanel = new JPanel(new GridLayout(0, 1, 0, 16));
+        JPanel actionButtonsPanel = new JPanel(new MigLayout("wrap 1, gapy 16", "fill"));
 	   	actionButtonsPanel.setOpaque(false);
 	   	actionsPanel.add(actionButtonsPanel);
-	   	
+
 	   	actionButtonsPanel.add(new JButton(documentActions.importClipboard));
 	   	actionButtonsPanel.add(new JButton(documentActions.importDocument));
 	   	actionButtonsPanel.add(new JButton(wordLookupAction));
-	   	
+
+        actionButtonsPanel.add(new JSeparator());
+
+        actionButtonsPanel.add(new JButton(new HyperlinkAction("main.menu.homepage")));
+
 	    return actionsPanel;
     }
 
